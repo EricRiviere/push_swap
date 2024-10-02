b98f65710ca457eb1220c3d9148fa525525db913
Eric Riviere <eric.riviere.ponsa@gmail.com>
2024-10-02 16:35:28 +0200

    main explaination added

--- PROJECT/src/main.c
+++ PROJECT/src/main.c
@@ -12,6 +12,20 @@
 
 #include "push_swap.h"
 
+/*EXPLAINATION:
+	-main: To initiate program
+		- Create empty nodes for both stacks (a, b)
+		- Protect the case in wich no argument or empty argument is provided
+		- If the number of arguments is 2 (aka we are recivieng a string with all arguments) we use the function replicate_argv to simulate argv
+		- Then we init the stack with the flag (free_arg in init stack function) to free simulated argv if argc = 2 (if needed else all remain the same)
+		- Check if the stack is already sorted (if so we free the stack)
+		- If the stack is not sorted:
+			- If we have 2 nodes we swap
+			- If we have 3 we apply base sort_three function
+			- Else we call sort_stack function that trigers all the optimization moves to sort
+		- Free the stack
+*/
+
 int	main(int argc, char *argv[])
 {
 	node	*a;
